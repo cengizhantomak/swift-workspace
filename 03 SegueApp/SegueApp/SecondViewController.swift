02cf8d93d6e658e32f46308cd16077d6b61d716e
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-08 17:54:32 +0300

    Temel Operasyonlar - Iki Ekranda Calisma

---
+++ 03 SegueApp/SegueApp/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  SegueApp
+//
+//  Created by Tuna Tomak on 14.08.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var myLabelSecond: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var myName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = myName
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
