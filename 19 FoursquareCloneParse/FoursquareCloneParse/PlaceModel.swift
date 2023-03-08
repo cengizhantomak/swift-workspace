5d65908fc88ffa854bca9780c5043ff547145fc0
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-08 18:52:08 +0300

    Foursquare Clone(Basic): Parse

---
+++ 19 FoursquareCloneParse/FoursquareCloneParse/PlaceModel.swift
@@ -0,0 +1,24 @@
+//
+//  PlaceModel.swift
+//  FoursquareCloneParse
+//
+//  Created by Kerem Tuna Tomak on 2.03.2023.
+//
+
+import Foundation
+import UIKit
+
+class PlaceModel {
+    
+    static let sharedInstance = PlaceModel()
+    
+    var placeName = ""
+    var placeType = ""
+    var placeAtmosphere = ""
+    var placeImage = UIImage()
+    var placeLatitude = ""
+    var placeLongitude = ""
+    
+    private init(){}
+    
+}
