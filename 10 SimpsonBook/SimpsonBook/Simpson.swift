f48c165d06313ba451bc2cbc30983b3f6977d892
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-08 18:04:21 +0300

    Siniflar ve OOP - OOP

---
+++ 10 SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,24 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Tuna Tomak on 22.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+}
