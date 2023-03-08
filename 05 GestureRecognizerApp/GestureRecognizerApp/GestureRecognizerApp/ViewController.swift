e3d4b3148744b0d7ca31ee38388ccb381aa46c19
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-08 17:55:27 +0300

    Temel Operasyonlar - Jest Algiliyicilari

---
+++ 05 GestureRecognizerApp/GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Tuna Tomak on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var isJames = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+    }
+    
+    @objc func changePic() {
+        if isJames == true {
+            imageView.image = UIImage(named: "lars")
+            myLabel.text = "Lars Ulrich"
+            isJames = false
+            
+        } else {
+            imageView.image = UIImage(named: "james")
+            myLabel.text = "James Hetfield"
+            isJames = true
+            
+        }
+        
+    }
+
+
+}
+
