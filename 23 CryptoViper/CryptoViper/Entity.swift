4f180fbf5d0b08eea628a266acba718d489df883
Cengizhan Tomak <98701769+cengizhantomak@users.noreply.github.com>
2023-03-17 10:35:08 +0300

    Viper & Protocol

---
+++ 23 CryptoViper/CryptoViper/Entity.swift
@@ -0,0 +1,15 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Kerem Tuna Tomak on 15.03.2023.
+//
+
+import Foundation
+
+//Struct
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
